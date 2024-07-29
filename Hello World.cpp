78410e516d3ac1ff40cf4e20561f7dc731553b33
SiddhantBhosale05 <siddhant.bhosale05@gmail.com>
2024-07-29 13:20:38 +0530

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,17 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Printing Hello World
+
+#include <iostream>
+using namespace std;
+
+int main() { 
+    
+    cout << "Hello World";
+   
+    return 0;
+} 
+
+
+//Output:
+//Hello World
